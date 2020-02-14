3ab98e201a67ac3eba2bdf1a119acb87ba869d3b
이창섭 <ventulus95@gmail.com>
2020-02-14 13:17:53 +0900

    Initial commit

---
+++ src/test/java/com/example/vuejs/VuejsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vuejs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuejsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
